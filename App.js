c738a49d4e4beb86ec3484f1fa018bd82ed55448
IidaPeltonen <n0peii00@students.oamk.fi>
2022-09-15 13:24:43 +0300

    valmis

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, Pressable } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [date, setDate] = useState(new Date()) 
+  const [show, setShow] = useState(false) 
+
+  const onChange = (e, selectedDate) => {
+    setShow(false)
+    const currentDate = selectedDate || date
+    setDate(currentDate)
+  }
+
+  const toggle = () => {
+    setShow(prevShow => !prevShow)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable
+        onPress={toggle}>
+          <Text>{date.getDate()}.{date.getMonth() + 1}.{date.getFullYear()}</Text>
+        </Pressable>
+      {Platform.OS === 'ios' && (
+        <DateTimePicker
+        style={{with:320}}
+        mode={'date'}
+        display='default'
+        value={date}
+        onChange={onChange}
+        />
+      )}
+      {Platform.OS === 'android' && (
+        <DateTimePicker
+        mode={'date'}
+        display='default'
+        value={date}
+        onChange={onChange}
+        />
+      )}
     </View>
   );
 }
